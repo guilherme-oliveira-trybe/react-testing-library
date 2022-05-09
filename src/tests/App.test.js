c38bd3f5043cbf2493629bbd9e37e4a50635f59d
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-05-09 20:42:02 -0300

    Requisito 1

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-// import userEvent from '@testing-library/user-event';
+import userEvent from '@testing-library/user-event';
 import renderWithRouter from '../services/renderWithRouter';
 import App from '../App';
 
@@ -24,5 +24,48 @@ describe('Requisito 1', () => {
 
     const linkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
     expect(linkFavorites).toBeInTheDocument();
+  });
+
+  test('se ao clicar em "Home" a página é redirecionada para "/"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+    userEvent.click(linkHome);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/');
+  });
+
+  test('se ao clicar em "About" a página é redirecionada para "/about"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+    userEvent.click(linkAbout);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/about');
+  });
+
+  test('ao clicar em "Favorite Pokémons" a pág é redirecionada para "/favorites"', () => {
+    const { history } = renderWithRouter(<App />);
+
+    const linkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavorites).toBeInTheDocument();
+    userEvent.click(linkFavorites);
+
+    const { pathname } = history.location;
+    expect(pathname).toBe('/favorites');
+  });
+
+  test('ao entrar em uma URL desconhecida, a pág é redirecionada para Not Found', () => {
+    const { history } = renderWithRouter(<App />);
+
+    history.push('/pagina-nao-encontrada');
+
+    const notFound = screen
+      .getByRole('heading', { level: 2, name: /page requested not found/i });
+    expect(notFound).toBeInTheDocument();
   });
 });
