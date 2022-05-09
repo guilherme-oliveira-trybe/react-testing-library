05fb1ad4f208cf4a66c471614294af7fb91aa38f
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-05-09 15:05:00 -0300

    Adiciona testes ref ao arquivo App.js

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -1,1 +1,28 @@
-test('', () => {});
+import React from 'react';
+import { screen } from '@testing-library/react';
+// import userEvent from '@testing-library/user-event';
+import renderWithRouter from '../services/renderWithRouter';
+import App from '../App';
+
+describe('Requisito 1', () => {
+  test('se o primeiro link, possui o texto "Home"', () => {
+    renderWithRouter(<App />);
+
+    const linkHome = screen.getByRole('link', { name: 'Home' });
+    expect(linkHome).toBeInTheDocument();
+  });
+
+  test('se o primeiro link, possui o texto "About"', () => {
+    renderWithRouter(<App />);
+
+    const linkAbout = screen.getByRole('link', { name: 'About' });
+    expect(linkAbout).toBeInTheDocument();
+  });
+
+  test('se o primeiro link, possui o texto "Favorite Pokémons"', () => {
+    renderWithRouter(<App />);
+
+    const linkFavorites = screen.getByRole('link', { name: 'Favorite Pokémons' });
+    expect(linkFavorites).toBeInTheDocument();
+  });
+});
