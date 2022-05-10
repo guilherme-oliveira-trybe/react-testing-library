592c763734ce8f564663140e84473bc2e36f4561
Guilherme Oliveira <guilherme.moliveira@hotmail.com>
2022-05-09 22:19:22 -0300

    Ajustando caminho renderWithRouter

--- src/tests/About.test.js
+++ src/tests/About.test.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { screen } from '@testing-library/react';
-import renderWithRouter from '../services/renderWithRouter';
+import renderWithRouter from '../renderWithRouter';
 import About from '../components/About';
 
 describe('Requisito 2', () => {
